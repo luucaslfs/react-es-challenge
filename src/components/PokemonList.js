3925a89741187a86ef10c4f5b5d048d7299057f4
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2023-07-29 23:53:25 -0300

    Adding Q7 answer

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -4,15 +4,20 @@ import axios from 'axios';
 const PokemonList = () => {
     const [pokemonList, setPokemonList] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [offset, setOffset] = useState(0);
+    const [hasMore, setHasMore] = useState(true);
 
     useEffect(() => {
         const fetchPokemonList = async () => {
             try {
+                setLoading(true);
                 const response = await axios.get(
-                    'https://pokeapi.co/api/v2/pokemon?limit=20'
+                    'https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}'
                 );
-                setPokemonList(response.data.results);
+                const newPokemonList = response.data.results;
+                setPokemonList((prevList) => [...prevList, ...newPokemonList]);
                 setLoading(false);
+                setHasMore(newPokemonList.length > 0); // Check if there are more Pokemon to be fetched
             } catch (error) {
                 console.error('Error fetching Pokemon list:', error);
                 setLoading(false);
@@ -20,7 +25,25 @@ const PokemonList = () => {
         };
 
         fetchPokemonList();
-    }, []);
+    }, [offset]);
+
+    const handleScroll = () => {
+        if (
+            window.innerHeight + document.documentElement.scrollTop ===
+            document.documentElement.offsetHeight
+        ) {
+            if (!loading && hasMore) {
+                setOffset((prevOffset) => prevOffset + 20);
+            }
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [loading, hasMore]);
 
     return (
         <div>
